9bb67dd71eeee0ce3bee08561e01fd8b43e17ded
outerspacebear <virjune@gmail.com>
2021-12-20 22:54:15 +0000

    Added timer (dev UI + logic) and mini-game start, end events

---
+++ Ultimate Temp Worker/Assets/Scripts/Common/MiniGameStarter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGameStarter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TODO: Maybe call after button press or something, instead od in Start()?
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        MiniGameEvents.miniGameStartedEvent.Invoke();
+    }
+}
---
+++ Ultimate Temp Worker/Assets/Scripts/Common/MiniGameTimer.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MiniGameTimer : MonoBehaviour
+{
+    private void Awake()
+    {
+        MiniGameEvents.miniGameStartedEvent.AddListener(SetHasGameStarted);
+    }
+
+    private void Update()
+    {
+        if(!hasGameStarted)
+        {
+            return;
+        }
+
+        totalTimeElapsed += Time.deltaTime;
+        UpdateUI();
+
+        if(totalTimeElapsed >= GlobalConsts.MiniGameDurationInSeconds)
+        {
+            OnTimeUp();
+        }
+    }
+
+    private void OnTimeUp()
+    {
+        MiniGameEvents.miniGameEndedEvent.Invoke();
+        Destroy(this);
+    }
+
+    private void SetHasGameStarted()
+    {
+        hasGameStarted = true;
+    }
+
+    bool hasGameStarted = false;
+    float totalTimeElapsed = 0;
+
+    //======================================
+
+    public GameObject clockHand;
+
+    void UpdateUI()
+    {
+        if (!clockHand)
+            return;
+
+        var elapsedTimePercent = (totalTimeElapsed * 100) / GlobalConsts.MiniGameDurationInSeconds;
+
+        var targetClockHandAngle = 360 - ((elapsedTimePercent / 100) * 360);
+        var currentClockHandAngle = clockHand.transform.rotation.eulerAngles.z;
+        var angleToRotateBy = targetClockHandAngle - currentClockHandAngle;
+
+        clockHand.transform.Rotate(transform.forward, angleToRotateBy);
+    }
+}
