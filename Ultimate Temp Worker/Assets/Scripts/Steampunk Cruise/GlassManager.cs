79a37239156ee87c9aa789e05e447514b093c9a6
dine-j <amandine.jala@gmail.com>
2021-12-21 08:53:23 +0000

    Work on colors

--- Ultimate Temp Worker/Assets/Scripts/Steampunk Cruise/GlassManager.cs
+++ Ultimate Temp Worker/Assets/Scripts/Steampunk Cruise/GlassManager.cs
@@ -14,11 +14,11 @@ public class GlassManager : MonoBehaviour
     }
 
     public List<GlassPosition> spawnablePositions;
-    public Dictionary<string, Transform> test;
     public GameObject prefabGlass;
 
     private GameObject currentGlass;
     private GlassPosition currentPosition;
+    private Color cocktailColor = Color.black;
 
     // Start is called before the first frame update
     void Start()
@@ -26,11 +26,6 @@ public class GlassManager : MonoBehaviour
         InitGlassAtRandom();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void InitGlassAtRandom() 
     {
@@ -67,50 +62,107 @@ public class GlassManager : MonoBehaviour
 
     public void FillWithRed()
     {
-        foreach (var renderer in currentGlass.GetComponentsInChildren<Renderer>())
-        {
-            if (renderer.tag == "Cocktail" && currentPosition.name == "Red")
-            {
-                renderer.material.color = Color.red;
-                renderer.enabled = true;
-            }
-        }
+        FillWithColor("Red");
     }
 
     public void FillWithYellow()
     {
-        foreach (var renderer in currentGlass.GetComponentsInChildren<Renderer>())
-        {
-            if (renderer.tag == "Cocktail" && currentPosition.name == "Yellow")
-            {
-                renderer.material.color = Color.yellow;
-                renderer.enabled = true;
-            }
-        }
+        FillWithColor("Yellow");
     }
 
     public void FillWithBlue()
     {
-        foreach (var renderer in currentGlass.GetComponentsInChildren<Renderer>())
-        {
-            if (renderer.tag == "Cocktail" && currentPosition.name == "Blue")
-            {
-                renderer.material.color = Color.blue;
-                renderer.enabled = true;
-            }
-        }
+        FillWithColor("Blue");
     }
 
    
     public void FillWithWhite()
     {
+        FillWithColor("White");
+    }
+
+    private void FillWithColor(string colorName)
+    {
         foreach (var renderer in currentGlass.GetComponentsInChildren<Renderer>())
         {
-            if (renderer.tag == "Cocktail" && currentPosition.name == "White")
+            if (renderer.tag == "Cocktail" && currentPosition.name == colorName)
             {
-                renderer.material.color = Color.white;
+                var newColor = GetNewColor(cocktailColor, GetColorBasedOnName(colorName));
+                cocktailColor = newColor;
+                renderer.material.color = cocktailColor;
                 renderer.enabled = true;
             }
+        }
+    }
+
+    private void MoveGlass()
+    {
+
+    }
+
+    static Color GetColorBasedOnName(string colorName)
+    {
+        switch (colorName)
+        {
+            case "Red":
+                return Color.red;
+            case "Yellow":
+                return Color.yellow;
+            case "Blue":
+                return Color.blue;
+            case "White":
+                return Color.white;
+            default:
+                return Color.black;
+        }
+    }
+
+    private Color GetNewColor(Color baseColor, Color fillingColor)
+    {
+        if (baseColor == Color.black)
+        {
+            return fillingColor;
+        }
+
+        if ((baseColor == Color.red && fillingColor == Color.red) || (fillingColor == Color.red && baseColor == Color.red))
+        {
+            return Color.red;
+        }
+        else if ((baseColor == Color.red && fillingColor == Color.yellow) || (fillingColor == Color.red && baseColor == Color.yellow))
+        {
+            return new Color(1.0f, 0.64f, 0.0f); // orange
+        }
+        else if ((baseColor == Color.red && fillingColor == Color.blue) || (fillingColor == Color.red && baseColor == Color.blue))
+        {
+            return new Color(1.27f, 0.63f, 1.91f); // purple
+        }
+        else if ((baseColor == Color.red && fillingColor == Color.white) || (fillingColor == Color.red && baseColor == Color.white))
+        {
+            return new Color(1.91f, 0.63f, 1.91f); // pink
+        }
+        else if ((baseColor == Color.blue && fillingColor == Color.yellow) || (fillingColor == Color.blue && baseColor == Color.yellow))
+        {
+            return Color.green;
+        }
+        else if ((baseColor == Color.blue && fillingColor == Color.white) || (fillingColor == Color.blue && baseColor == Color.white))
+        {
+            return Color.cyan;
+        }
+        else if ((baseColor == Color.blue && fillingColor == Color.blue) || (fillingColor == Color.blue && baseColor == Color.blue))
+        {
+            return Color.blue;
+        }
+        else if ((baseColor == Color.white && fillingColor == Color.white) || (fillingColor == Color.white && baseColor == Color.white))
+        {
+            return Color.white;
+        }
+        else if ((baseColor == Color.yellow && fillingColor == Color.yellow) || (fillingColor == Color.yellow && baseColor == Color.yellow))
+        {
+            return Color.yellow;
+        }
+        else
+        {
+            return Color.black;
         }
     }
 }
