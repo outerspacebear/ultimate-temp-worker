ce13cf2673b803d784acf5f2b097c1e1c0e031e9
outerspacebear <virjune@gmail.com>
2021-12-21 16:32:02 +0000

    Added planet generator

--- Ultimate Temp Worker/Assets/Scripts/Creature Breeder/Non-scripts/Planet.cs
+++ Ultimate Temp Worker/Assets/Scripts/Creature Breeder/Non-scripts/Planet.cs
@@ -5,7 +5,38 @@ using UnityEngine.UI;
 
 public class PlanetComposition
 {
-    public PlanetComposition(Dictionary<Element, float> elementalComposition)
+    public static PlanetComposition GetRandomComposition()
+    {
+        float remainingPercent = 100f;
+
+        Dictionary<Element, float> composition = new Dictionary<Element, float>();
+
+        Shuffle(ElementUtil.AllElements);
+        foreach(var element in ElementUtil.AllElements)
+        {
+            float percentForElement = Random.Range(0, remainingPercent + 1);
+            composition.Add(element, percentForElement);
+
+            remainingPercent -= percentForElement;
+        }
+
+        return new PlanetComposition(composition);
+    }
+
+    private static void Shuffle<T>(IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, list.Count);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+
+    private PlanetComposition(Dictionary<Element, float> elementalComposition)
     {
         this.elementalComposition = elementalComposition;
     }
@@ -15,6 +46,12 @@ public class PlanetComposition
 
 public class Planet
 {
-    PlanetComposition composition;
-    Sprite sprite;
+    public Planet(PlanetComposition composition, Sprite sprite)
+    {
+        this.composition = composition;
+        this.sprite = sprite;
+    }
+
+    public PlanetComposition composition { get; }
+    public Sprite sprite { get; }
 }
